0cf9e02efc2f640988a5d77ef44ae50524921c34
Ben Sudbury <ben@rebas.com.au>
2014-10-27 06:09:49 +1100

    updated scribble plotter example to work with new syntax

--- examples/scribbleplotter/scribbleplotter.js
+++ examples/scribbleplotter/scribbleplotter.js
@@ -1,11 +1,21 @@
 var fs = require('fs')
   , sketch = __dirname + '/scribbleplotter.pde'
-  , processing = require('../../');
+  , Processing = require('../../')
+  , Canvas = require("canvas");
 
-fs.readFile(sketch, function(err, data) {
-  var p5 = processing.createInstance(data, sketch)
+ 
+
+ var canvas = new Canvas(200,200);
+
+
+fs.readFile(sketch, {"encoding": "utf-8"}, function(err, data) {
+	if(err){
+		console.log(err);
+	}
+	// console.log(data);
+  var p5 = new Processing(canvas, data)
     , out = fs.createWriteStream(__dirname + '/scribbleplotter.png')
-    , stream = p5.canvas.createPNGStream();
+    , stream = canvas.createPNGStream();
 
   stream.pipe(out);
 });
